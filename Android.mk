d5fe1220d5bdea9b7c92be07e72eedd1833e5b2b
Ivan Meler <i_ivan@windowslive.com>
2017-11-09 20:33:27 +0100

    libdisplay --> libexynosdisplay

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@ ifeq ($(TARGET_SOC), exynos8890)
 exynos8890_dirs := \
 	mobicore \
 	gralloc \
-	libdisplaymodule \
+	libexynosdisplaymodule \
 	libhwcutilsmodule \
 	libhdmimodule \
 	libhwjpeg \
